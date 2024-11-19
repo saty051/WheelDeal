f6391980215e39e847b54455d076c0009a74c446
Satyam <satyamverma0102@gmail.com>
2024-11-19 10:16:21 +0530

    Implement CRUD Operations for Makes of Vehicles (Part-1)

---
+++ Controllers/MakeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WheelDeal.AppDBContext;
+using WheelDeal.Models;
+
+namespace WheelDeal.Controllers
+{
+    public class MakeController : Controller
+    {
+        private readonly WheelDealDbContext _dbContext;
+
+        public MakeController(WheelDealDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_dbContext.Makes.ToList());
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -5,7 +5,17 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-builder.Services.AddDbContext<WheelDealDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddDbContext<WheelDealDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Safely register the length constraint
+builder.Services.Configure<RouteOptions>(options =>
+{
+    if (!options.ConstraintMap.ContainsKey("length"))
+    {
+        options.ConstraintMap.Add("length", typeof(Microsoft.AspNetCore.Routing.Constraints.LengthRouteConstraint));
+    }
+});
 
 var app = builder.Build();
 
@@ -13,7 +23,7 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    // The default HSTS value is 30 days. Consider changing for production scenarios.
     app.UseHsts();
 }
 
@@ -24,6 +34,14 @@ app.UseRouting();
 
 app.UseAuthorization();
 
+// Add custom route
+//app.MapControllerRoute(
+//    name: "ByYearMonth",
+//    pattern: "make/bikes/{year:int:length(4)}/{month:int:range(1,12)}",
+//    defaults: new { controller = "Make", action = "ByYearMonth" }
+//);
+
+// Default route
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
