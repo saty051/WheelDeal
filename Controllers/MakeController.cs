986f22d01f482197443f722e37481fc23be47544
Satyam <satyamverma0102@gmail.com>
2024-11-23 18:47:57 +0530

    Implement CRUD Operations for Makes of Vehicles (Part-3)

--- Controllers/MakeController.cs
+++ Controllers/MakeController.cs
@@ -17,5 +17,39 @@ namespace WheelDeal.Controllers
         {
             return View(_dbContext.Makes.ToList());
         }
+
+        //HTTP Get Method
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Make make)
+        {
+            if (ModelState.IsValid)
+            {
+                _dbContext.Add(make);
+                _dbContext.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(make);
+        }
+
+        // Post Method
+        [HttpPost]
+        public IActionResult Delete(int id)
+        {
+            var make = _dbContext.Makes.Find(id);
+            if(make == null)
+            {
+                return NotFound();
+            }
+
+            _dbContext.Remove(make);
+            _dbContext.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
